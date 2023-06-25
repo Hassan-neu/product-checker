c9ca7a757faad96b81304f81ea78409567a87e2a
Hassan Adeyemi <hassanadeyemi01@gmail.com>
2023-06-25 11:31:41 +0100

    :package: moved data fetching to chat directory

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,14 @@
-// "use client";
-import Image from "next/image";
-import Message from "@/components/message";
 import SignIn from "@/components/signin";
 import Chat from "@/components/chat";
 import Input from "@/components/messageInput";
+import ScrollElement from "@/components/scrollElement";
 import { AuthOptions } from "@/libs/auth";
 import { getServerSession } from "next-auth";
-import ScrollElement from "@/components/scrollElement";
-// import { useState, useEffect, useRef } from "react";
 export default async function Home() {
     const session = await getServerSession(AuthOptions);
-    const res = await fetch("http://localhost:3000/api/messages", {
-        method: "GET",
-        cache: "no-store",
-    });
-    const messages = await res.json();
     return (
         <main className="flex min-h-screen flex-col justify-between px-8 gap-4">
-            <>
-                {session ? (
-                    <Chat messages={messages} session={session} />
-                ) : (
-                    <SignIn />
-                )}
-            </>
+            <>{session ? <Chat session={session} /> : <SignIn />}</>
             <ScrollElement />
             <Input />
         </main>
--- src/components/chat.js
+++ src/components/chat.js
@@ -1,7 +1,11 @@
-"use client";
 import React from "react";
 import Message from "./message";
-const Chat = ({ messages, session }) => {
+const Chat = async ({ session }) => {
+    const res = await fetch("http://localhost:3000/api/messages", {
+        method: "GET",
+        cache: "no-store",
+    });
+    const messages = await res.json();
     return (
         <div className="flex h-full flex-col justify-between gap-[7px]">
             {messages.map((message) => (
